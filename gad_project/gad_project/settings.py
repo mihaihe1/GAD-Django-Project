284cbe021f70333eb5d4f9b9662769269bd27161
mihaihe1 <44977942+mihaihe1@users.noreply.github.com>
2021-08-05 23:50:48 +0300

    Update settings.py

--- gad_project/gad_project/settings.py
+++ gad_project/gad_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=w+lm0nf44io^^anx)050olm@5s5dva+f6eh+2$$0gv=c8%&s2'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
